073bed7ed1d6457c7d1df11cb520ae4bf4aec22f
Jalaj7d <116143485+Jalaj7d@users.noreply.github.com>
2022-10-19 13:17:16 +0530

    Create Armstrong number.cpp

---
+++ Armstrong number.cpp
@@ -0,0 +1,43 @@
+#include <cmath>
+
+#include <cstdio>
+
+#include <vector>
+
+#include <iostream>
+
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    int num, onum, r, re = 0;
+
+    cout<<"Enter a three-digit integer: ";
+
+    cin>>num;
+
+    onum = num;
+
+    while(onum != 0){
+
+        r = onum % 10;
+
+        re += r*r*r;
+
+        onum /= 10;
+
+    }
+
+    if (re == num)
+
+        cout<<num<<" is an Armstrong number.";
+
+    else
+
+        cout<<num<<" is not an armstrong number.";
+
+    return 0;
+
+}
